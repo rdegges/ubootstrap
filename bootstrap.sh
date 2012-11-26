cf6649d7bc302c216203b33a340b060a896ecc03
Randall Degges <rdegges@gmail.com>
2012-11-25 23:47:58 -0800

    Adding heroku-accounts stuff.

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,7 +30,6 @@ sudo aptitude -y install zsh
 # Install oh-my-zsh.
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 chsh -s /bin/zsh
-rm -f ~/.zshrc
 
 # Install development stuff (Python).
 sudo aptitude -y install python-dev python-pip build-essential
@@ -44,6 +43,9 @@ ssh-keygen -t rsa
 # Install the Heroku toolbelt.
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
+# Install heroku-accounts.
+heroku plugins:install git://github.com/ddollar/heroku-accounts.git
+
 # Install RVM (so I can use gems).
 curl -L https://get.rvm.io | bash -s stable --ruby
 
@@ -51,12 +53,19 @@ curl -L https://get.rvm.io | bash -s stable --ruby
 git clone git@github.com:rdegges/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles && git submodule init
 cd ~/.dotfiles && git submodule update
+rm -f ~/.gitconfig
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+rm -rf ~/.pip
 ln -s ~/.dotfiles/pip ~/.pip
+rm -f ~/.tmux.conf
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+rm -rf ~/.vim
 ln -s ~/.dotfiles/vim ~/.vim
+rm -f ~/.vimrc
 ln -s ~/.dotfiles/vimrc ~/.vimrc
+rm -f ~/.vim.scripts
 ln -s ~/.dotfiles/vim.scripts ~/.vim.scripts
+rm -f ~/.zshrc
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 # Install all Vim modules.
